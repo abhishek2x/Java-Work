2a0a40e176101b20e69f682bedc1943f55ee5ef5
abhishek2x <abhisheksrivastavabbn@gmail.com>
2020-12-21 10:24:59 +0530

    Java netbeans prev

---
+++ Java programs-netbeans/JavaApplication1/src/Swing5/java.java
@@ -0,0 +1,81 @@
+package Swing5;
+
+
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+public class Swing5 implements ActionListener
+{
+    JFrame j1;
+    JLabel l1,l2,l3;
+    JTextField t1,t2,t3;
+    JButton b;
+    Swing5()
+    {
+        j1=new JFrame("Sample Frame");
+        l1=new JLabel("personId");
+        l1.setBounds(50,100,100,30);
+        l2=new JLabel("orderId");
+        l2.setBounds(50,150,100,30);
+        l3=new JLabel("orderNumber");
+        l3.setBounds(50,200,100,30);
+        
+        t1=new JTextField();
+        t1.setBounds(150,100,150,20);
+        t2=new JTextField();
+        t2.setBounds(150,150,150,20);
+        t3=new JTextField();
+        t3.setBounds(150,200,150,20);
+        
+        b=new JButton("click");
+        b.setBounds(50,300,100,100);
+        b.addActionListener(this);
+        j1.add(l1);
+        j1.add(l2);
+        j1.add(l3);
+        j1.add(t1);
+        j1.add(t2);
+        j1.add(t3); 
+        j1.add(b);
+        j1.setSize(400,400);
+        j1.setLayout(null);
+        j1.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==b)
+        {
+            int p=Integer.parseInt(t1.getText());
+            int o=Integer.parseInt(t2.getText());
+            int on=Integer.parseInt(t3.getText());
+            System.out.println(p+" "+o+" "+on);
+            Connection c=null;
+            String url="jdbc:mysql://localhost:3306/sample1";
+            try
+            {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println("Driver Loaded");
+        c=DriverManager.getConnection(url,"root", "#Saksh02");
+        System.out.println("connection established");
+        Statement stmt=c.createStatement();
+        String sql ="insert into orders values ( '" +p+ "' , '" +o+ "' , '" +on+ "' )";
+        stmt.executeUpdate(sql);
+	c.close();
+            }
+           catch(SQLException e1)
+        {
+            System.out.println("connection not established"+e1);
+        }
+           catch(ClassNotFoundException e2)
+        {
+            System.out.println("Driver not Loaded");
+    }
+        }
+    }
+    public static void main(String args[])
+    {
+        Swing5 ob=new Swing5();
+    }
+}
---
+++ Java programs-netbeans/LearnSQL/src/learnsql/MySQLConn.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+public class MySQLConn 
+{
+    public static void main(String args[])
+    {
+        String s="create table Tb1(col1 int, col2 varchar(20))";
+        Connection c=null;
+       String url="jdbc:mysql://localhost:3306/college";
+        try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println("Driver Loaded");
+        c=DriverManager.getConnection(url,"root", "blackrider12345");
+        System.out.println("connection established");
+        Statement stmt=c.createStatement();
+        ResultSet re=stmt.executeQuery("select * from student_details");
+        while(re.next())
+        {
+            System.out.println(re.getString(1)+"  "+re.getString(2)+"  "+re.getString(3)+"  "+re.getString(4)); 
+        }
+        
+//        ResultSet r1=stmt.executeQuery("select * from Employees");
+//        while(r1.next())
+//        {
+//            System.out.println(r1.getString(1)+" "+r1.getString(2)+" "+r1.getString(3));
+//        }
+        System.out.println("sql updated");
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.out.println("Driver not Loaded");
+    }
+        catch(SQLException e)
+        {
+            System.out.println("connection not established");
+        }
+        try{
+        c.close();
+          }
+        catch(SQLException e)
+        {
+          System.out.println("error");
+        }
+    }
+}
+
+
